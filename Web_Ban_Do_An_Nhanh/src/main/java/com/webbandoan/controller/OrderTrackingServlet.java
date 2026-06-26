2050cb09ab924d433cf470f1148fdcf850a261f7
nTruong257 <22130306@st.hcmuaf.edu.vn>
2026-06-26 14:20:00 +0700

    feat(UC9): handle alt branch for non-existent order exceptions
    Bổ sung rẽ nhánh logic if (order == null) trong Controller để xử lý ngoại lệ khi mã đơn hàng không tồn tại trong hệ thống.

--- Web_Ban_Do_An_Nhanh/src/main/java/com/webbandoan/controller/OrderTrackingServlet.java
+++ Web_Ban_Do_An_Nhanh/src/main/java/com/webbandoan/controller/OrderTrackingServlet.java
@@ -1,4 +1,4 @@
 package com.webbandoan.controller;
 import javax.servlet.http.*;
-public class OrderTrackingServlet extends HttpServlet { /* logic 1 */ }
-// commit 2
+public class OrderTrackingServlet extends HttpServlet { /* logic 1 + exception handling */ }
+// commit 3
