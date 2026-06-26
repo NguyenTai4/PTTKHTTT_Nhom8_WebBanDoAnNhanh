d195cb17da4b2a34eb26a5156fbfe9cca31f9a52
nTruong257 <22130306@st.hcmuaf.edu.vn>
2026-06-26 11:05:00 +0700

    feat(UC9): implement inputOrderCode logic in controller
    Viết logic nhận tham số orderCode từ giao diện, gọi hàm queryOrder từ DAO và điều hướng kết quả thành công sang trang order_details.jsp.

--- Web_Ban_Do_An_Nhanh/src/main/java/com/webbandoan/controller/OrderTrackingServlet.java
+++ Web_Ban_Do_An_Nhanh/src/main/java/com/webbandoan/controller/OrderTrackingServlet.java
@@ -1,4 +1,4 @@
 package com.webbandoan.controller;
 import javax.servlet.http.*;
-public class OrderTrackingServlet extends HttpServlet {}
-// commit 1
+public class OrderTrackingServlet extends HttpServlet { /* logic 1 */ }
+// commit 2
